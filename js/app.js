587d0d154b7fa3e4fddecd19280e1f09221c96f4
CleverShovel <quest201114>
2018-05-21 23:42:23 +0300

    Added commands to snake

--- js/app.js
+++ js/app.js
@@ -8,40 +8,100 @@ document.onkeydown = function (event) {
 		case 37:
 			num_direction = (size+num_direction-1)%size;
 			direction = directions[num_direction];
-			console.log(direction);
+			isMoving = false;
 			break;
 		
 		case 39:
 			num_direction = (size+num_direction+1)%size;
 			direction = directions[num_direction];
-			console.log(direction);
+			isMoving = false;
 			break;
 	
 		case 38:
 			direction = directions[num_direction];
-			console.log(direction);
+			isMoving = true;
 			break;
 		
 		default:
 			direction = "null";
 			break;
 	}
+	
+	document.getElementById('direction').textContent = direction;
 }
 
 function reset()
 {
 	direction = "down";
 	num_direction = 2;
+	
+	isMoving = true;
 }
 
 btn.onclick = function()
 {
 	start.style.display = "none";
 	reset();
+	document.getElementById('direction').textContent = direction;
 	drawModule.init();
 }
+
+function addCommand(command)
+{
+	program = document.getElementById('program');
+	if (!program.textContent.trim()) program.textContent=command;
+	else program.textContent += '\r\n'+command;
+}
+
+forward.onclick = function()
+{
+	addCommand('forward');
+}
+
+right.onclick = function()
+{
+	addCommand('right');
+}
+
+left.onclick = function()
+{
+	addCommand('left');
+}
+
+function* changeDirection(inText){
+	var size = directions.length;
+	var lines = inText.split(/\s*\n\s*/);
+	for (var j = 0; j < lines.length; j++) {
+		if (lines[j].trim()==="right"){
+			num_direction = (size+num_direction+1)%size;
+			direction = directions[num_direction];
+			isMoving = false;
+		}
+		else if (lines[j].trim()==="left"){
+			num_direction = (size+num_direction-1)%size;
+			direction = directions[num_direction];
+			isMoving = false;
+		}
+		else if (lines[j].trim()==="forward"){
+			direction = directions[num_direction];
+			isMoving = true;
+		}
+		yield direction;
+	}
+}
+
+go.onclick = function()
+{
+	var iter = changeDirection(document.getElementById('program').textContent);
+	setInterval(function(iter) {
+		iter.next();
+	}, 100, iter);
+}
+
+/*var commandRead = setInterval(function(iter) {
+  iter.next();
+}, 100, iter);*/
 
 canvas.addEventListener("gameover", function(event) {
 	start.style.display = "block";
 	}, false);
-	
--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,31 @@
+var size = {
+  width: window.innerWidth || document.body.clientWidth,
+  height: window.innerHeight || document.body.clientHeight
+}
+
 var start = document.getElementById('start');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var snakeSize = 10; 
-var w = 350;
-var h = 350;
+
+var area = document.getElementById('area');
+var home = document.getElementById('home');
+var home_w = size.width*(parseInt(area.style.width)/100)*(parseInt(home.style.width)/100);
+var home_h = size.height*(parseInt(area.style.height)/100)*(parseInt(home.style.height)/100);
+
+canvas.width = home_w;
+canvas.height = home_h;
+var w = canvas.width;
+var h = canvas.height;
+
+
 var score = 0;
 var snake;
-var snakeSize = 10;
+var snakeSize = size.width/100;
 var food;
 
 var direction = "down";
 var directions = ["up","right","down","left"];
 var num_direction = 2;
+
+var isMoving = true;
